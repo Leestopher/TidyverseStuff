0dea31552f29efd65ae1d8f56c4711c9523bc5bc
Leestopher <chreestopherlee@gmail.com>
2022-03-31 22:54:17 -0400

    Pretty crappy first plot..

--- 3-29-22/collegiatesports.R
+++ 3-29-22/collegiatesports.R
@@ -1,4 +1,5 @@
-install.packages("tidytuesdayR")
+# install.packages("tidytuesdayR")
+install.packages("httpgd")
 
 library(dplyr)
 library(tidyverse)
@@ -10,9 +11,21 @@ library(ggthemes)
 library(scales)
 library(ggplot2)
 library(tidytuesdayR)
+library(leaflet)
+
+httpgd::hgd()
+httpgd::hgd_browse()
 
 tuesdata <- tidytuesdayR::tt_load('2022-03-29')
 
 sports <- tuesdata$sports
 
 sports
+
+gasports <- sports %>% 
+    filter(state_cd == "GA" & sports == "Bowling" & total_rev_menwomen > 0)
+
+gasportsplot <- ggplot(gasports, aes(x = institution_name, y = total_rev_menwomen)) +
+    geom_boxplot()
+
+gasportsplot
