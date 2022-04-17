92a66ff992c59ac64e5d04f3239af44babe18853
Leestopher <chreestopherlee@gmail.com>
2022-04-16 23:16:54 -0400

    Small upload of TT

---
+++ 4-12-22_Pollution/pollution.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(tidyverse)
+library(sf)
+library(jsonlite)
+library(leaflet)
+library(tibbletime)
+library(ggthemes)
+library(scales)
+library(ggplot2)
+library(tidytuesdayR)
+library(leaflet)
+
+httpgd::hgd()
+httpgd::hgd_browse()
+
+tuesdata <- tidytuesdayR::tt_load('2022-04-12')
+
+in_poll <- tuesdata$indoor_pollution
+
+contpoll <- in_poll %>%
+    filter(is.na(Code))
+
+countrypoll <- in_poll %>%
+    filter(Code != "")
+
+contpoll
